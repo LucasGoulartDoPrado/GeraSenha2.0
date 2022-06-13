f3d9f53e16be73b45e2a0829ad785dbd94bfc565
Lucas Goulart do Prado <34442250+LucasGoulartDoPrado@users.noreply.github.com>
2022-06-13 16:08:58 -0300

    Update GeraSenha2.0.py
    Ajustado a função e realizado testes com datas 31 e menores ou igual a 30

--- GeraSenha2.0.py
+++ GeraSenha2.0.py
@@ -1,4 +1,3 @@
-from calendar import month
 from datetime import datetime as dt
 
 
@@ -7,18 +6,18 @@ def calculaSenha(data):
         digito = str(8)
     else:
         digito = str(9)
-    data_2 = data.replace(day=(data.day+1), month=(data.month-1))
+
+    if data.day > 30:
+        data_2 = data.replace(day=1, month=(data.month-1))
+    else:
+        data_2 = data.replace(day=(data.day+1), month=(data.month-1))
+
     dia_str = data_2.strftime("%d")[::-1]
     mes = data_2.strftime("%m")
     senha = dia_str + mes + digito
     print(senha)
 
 
-#valor = input('Digite a data(DD/MM/AAAA): ')
-valor = '31/05/2021'
-dia = dt.strptime(valor, '%d').date.day
-mes = dt.strptime(valor, '%m').date.month
-print(type(dia.days))
-# print(data.day)
-# print(data.Month)
-# calculaSenha(data)
+valor_str = input('Digite a data(DD/MM/AAAA): ')
+valor = dt.strptime(valor_str, '%d/%m/%Y').date()
+calculaSenha(valor)
